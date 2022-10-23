b53f75e36fce9f80ce3e46e91cfb0f337839734e
Lex Nousagi <alexmiyin@gmail.com>
2022-10-23 21:20:16 +0300

    refactor(projects): move css in one file and remove `ProjectsList` component

--- components/project-item.tsx
+++ components/project-item.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import s from '@/styles/project-item.module.scss'
+import s from '@/styles/projects.module.scss'
 
 interface Props {
   name: string
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,13 +1,13 @@
 import Container from '@/components/container'
 import ProjectItem from '@/components/project-item'
-import ProjectsList from '@/components/projects-list'
 import type { NextPage } from 'next'
+import s from '@/styles/projects.module.scss'
 
 const ProjectsPage: NextPage = () => {
   return (
     <Container title="Projects">
       <p>Stuff I built using React, React Native, Next.js, and TypeScript</p>
-      <ProjectsList>
+      <div className={s.list}>
         <ProjectItem
           name="KYI"
           description="Find all Kyiv events in one app"
@@ -32,7 +32,7 @@ const ProjectsPage: NextPage = () => {
           href="https://mariner.kyivangels.com"
           year={2022}
         />
-      </ProjectsList>
+      </div>
     </Container>
   )
 }
