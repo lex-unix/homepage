9ea9b1f5ad2c53fbf47c8e4e0226d84970852727
Lex Nousagi <alexmiyin@gmail.com>
2022-11-05 22:13:11 +0200

    chore(sanity): add config

---
+++ sanity.config.ts
@@ -0,0 +1,55 @@
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { markdownSchema } from 'sanity-plugin-markdown'
+
+export default defineConfig({
+  name: 'default',
+  title: 'homepage',
+  projectId: 'i4m7aez4',
+  dataset: 'production',
+  plugins: [deskTool(), markdownSchema()],
+  schema: {
+    types: [
+      {
+        name: 'post',
+        type: 'document',
+        title: 'Post',
+        fields: [
+          {
+            name: 'title',
+            title: 'Title',
+            type: 'string'
+          },
+          {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+              source: 'title'
+            }
+          },
+          {
+            name: 'coverImage',
+            title: 'Cover Image',
+            type: 'image'
+          },
+          {
+            name: 'description',
+            title: 'description',
+            type: 'string'
+          },
+          {
+            name: 'content',
+            title: 'Content',
+            type: 'markdown'
+          },
+          {
+            name: 'date',
+            title: 'Date',
+            type: 'datetime'
+          }
+        ]
+      }
+    ]
+  }
+})
