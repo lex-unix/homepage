cfbc9f0877d5f57416dc44801496130e0e82767e
Lex Nousagi <alexmiyin@gmail.com>
2022-11-08 20:06:15 +0200

    feat(mdx): set up `mdx-remote`

---
+++ lib/mdx.ts
@@ -0,0 +1,15 @@
+import { serialize } from 'next-mdx-remote/serialize'
+import remarkGfm from 'remark-gfm'
+import rehypePrism from 'rehype-prism-plus'
+
+export async function mdxToHtml(source: string) {
+  const mdxSource = await serialize(source, {
+    mdxOptions: {
+      remarkPlugins: [remarkGfm],
+      rehypePlugins: [rehypePrism],
+      format: 'mdx'
+    }
+  })
+
+  return mdxSource
+}
