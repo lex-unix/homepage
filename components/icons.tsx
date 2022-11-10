2179bae404341ec52a4199df1ba7db6786998c0b
Lex Nousagi <alexmiyin@gmail.com>
2022-11-10 23:07:25 +0200

    fix(navbar): icon size and padding

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,7 +1,7 @@
 export const CommandIcon = () => (
   <svg
-    width="32"
-    height="32"
+    width="28"
+    height="28"
     viewBox="0 0 32 32"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
