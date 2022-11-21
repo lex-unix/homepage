b67ea251395769596feddaf055ba8ee947f000a5
Lex Nousagi <alexmiyin@gmail.com>
2022-11-21 23:30:48 +0200

    chore(next): add security headers

--- next.config.js
+++ next.config.js
@@ -4,7 +4,42 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['mosaic.scdn.co', 'i.scdn.co']
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders
+      }
+    ]
   }
 }
+
+const securityHeaders = [
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload'
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()'
+  }
+]
 
 module.exports = nextConfig
