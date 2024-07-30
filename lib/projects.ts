f9b8b485acbd660d212efe1dc47249a5a09f9bb0
Lex Miin <alexmiyin@gmail.com>
2024-07-30 16:24:29 +0300

    feat: add projects and articles to home page

---
+++ lib/projects.ts
@@ -0,0 +1,33 @@
+export const projects = [
+  {
+    name: 'Babyn Yar',
+    description:
+      'Government project in memory of the victims of the Babyn Yar tragedy',
+    href: 'https://babynyar.gov.ua',
+    year: 2023
+  },
+  {
+    name: 'acro',
+    description: 'Explore stunning architecture projects worldwide',
+    href: 'https://acro.lexunix.me',
+    year: 2023
+  },
+  {
+    name: 'Shelf',
+    description: 'App for tracking reading goals',
+    href: 'https://shelf.lexunix.me',
+    year: 2023
+  },
+  {
+    name: 'Pravda',
+    description: 'Ukrainian news extension for Raycast',
+    href: 'https://github.com/lex-unix/pravda-extension',
+    year: 2022
+  },
+  {
+    name: 'Mariner',
+    description: 'Website for luxury yachts and apartments in Croatia',
+    href: 'https://www.marinercroatia.net',
+    year: 2022
+  }
+]
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
-import type { NextPage } from 'next'
 import Container from '@/components/container'
 import s from '@/styles/index.module.scss'
 import Image from 'next/image'
 import profileImage from '../public/profile.png'
+import { getClient } from '@/lib/sanity-server'
+import { recentPostsQuery } from '@/lib/queries'
+import { Post } from '@/lib/types'
+import { InferGetStaticPropsType } from 'next'
+import { projects } from '@/lib/projects'
 
-const Home: NextPage = () => {
+export default function Home({
+  posts
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <Container description="Computer Science student from Kyiv, Ukraine. Building web and mobile applications">
       <div className={s.info}>
@@ -29,8 +35,52 @@ const Home: NextPage = () => {
           applications. Having interest in machine learning.
         </p>
       </div>
+      <div className={s.content}>
+        <section className={s.content}>
+          <h1>Projects</h1>
+          <div className={s.subcontent}>
+            {projects.slice(0, 2).map(p => (
+              <div>
+                <a href={p.href} target="_blank" className={s.subTitle}>
+                  {p.name}
+                </a>
+                <p className={s.subDesc}>{p.description}</p>
+              </div>
+            ))}
+            <div>
+              <a href="/projects" className={s.subTitle}>
+                All projects
+              </a>
+              <p className={s.subDesc}>Some stuff I've built</p>
+            </div>
+          </div>
+        </section>
+        <section className={s.content}>
+          <h1>Writing</h1>
+          <div className={s.subcontent}>
+            {posts.map(p => (
+              <div>
+                <a href={`/writing/${p.slug}`} className={s.subTitle}>
+                  {p.title}
+                </a>
+                <p className={s.subDesc}>{p.description}</p>
+              </div>
+            ))}
+            <div>
+              <a href="/writing" className={s.subTitle}>
+                All writing
+              </a>
+              <p className={s.subDesc}>Some stuff I write about occasionally</p>
+            </div>
+          </div>
+        </section>
+      </div>
     </Container>
   )
 }
 
-export default Home
+export const getStaticProps = async ({ preview = false }) => {
+  const posts: Post[] = await getClient(preview).fetch(recentPostsQuery)
+
+  return { props: { posts } }
+}
